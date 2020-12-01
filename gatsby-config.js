8e656ca81a80caca646f436f58ab32d21ef5627d
Aadhil Khan <66699051+aadhilkhan@users.noreply.github.com>
2020-12-01 16:33:00 +0500

    first setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Deviant Matter`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Aadhil Khan`,
+      summary: `who lives and works in Kerala, building next reality experiences.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `Design and Thinking for the Next Century.`,
+    siteUrl: `https://deviantmatter.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `imaadhilkhan`,
     },
   },
   plugins: [
